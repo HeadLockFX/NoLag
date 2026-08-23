7796235e5088d7edd6206f21ea977f3340f338f1
HeadLockFX <135324773+HeadLockFX@users.noreply.github.com>
2026-08-23 16:48:47 +0700

    Update UI_WRAPPER_AND_ICONS.lua

--- Library/UI_WRAPPER_AND_ICONS.lua
+++ Library/UI_WRAPPER_AND_ICONS.lua
@@ -2,4 +2,7 @@
 UI Wrapper & Icons
 (Obfuscated so game developers won't be able to see the Roblox asset IDs :3)
 If you want the list of icons, just contact me on Discord.
+
+Total Icons: 7,239
+Last Updated: 23/08/2026
 --]]
