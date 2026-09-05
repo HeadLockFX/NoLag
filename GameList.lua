4f2f35c2a84a00a5ef4a78d9a8609ff2cb98543b
HeadLockFX <135324773+HeadLockFX@users.noreply.github.com>
2026-09-05 12:20:59 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,3 +1,6 @@
 return {
-    [10200395747] = { url = "https://raw.githubusercontent.com/Mbeldoz/RobloxScript/refs/heads/main/Game/GAG2.lua" }, --Murder Mystery 2
+    [66654135] = "82787434147645891814", --Murder Mystery 2
+    [10539411000] = "79681433079132212865", --Clean All Leaves
+    [9584852943] = "59285826842846144554", --+1 Speed Escape Keyboard
+    [7326934954] = "69469422484484856217", --99 Nights in the Forest
 }
